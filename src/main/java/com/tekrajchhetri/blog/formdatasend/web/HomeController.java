261d6a7ae4396a0d36e3ae993a4fdcff40517742
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2020-01-11 13:02:13 +0200

    Userinfo object created for Mapping user object with form --corrected

--- src/main/java/com/tekrajchhetri/blog/formdatasend/web/HomeController.java
+++ src/main/java/com/tekrajchhetri/blog/formdatasend/web/HomeController.java
@@ -1,12 +1,14 @@
 package com.tekrajchhetri.blog.formdatasend.web;
 
 import com.tekrajchhetri.blog.formdatasend.UserInfo;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class HomeController {
 
     @RequestMapping("/index")
-    public String index(UserInfo userInfo){
+    public String index(Model model){
+        model.addAttribute("userInfo", new UserInfo());
         return "index";
     }
 }
