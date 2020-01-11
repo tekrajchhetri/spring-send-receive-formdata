739b047cdeaae59692e8fb7f0b2b9cdb2d704541
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2020-01-11 12:50:25 +0200

    spring default file

---
+++ src/main/java/com/tekrajchhetri/blog/formdatasend/FormdatasendApplication.java
@@ -0,0 +1,13 @@
+package com.tekrajchhetri.blog.formdatasend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormdatasendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormdatasendApplication.class, args);
+	}
+
+}
