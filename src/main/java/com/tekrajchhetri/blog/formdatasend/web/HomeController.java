c9ce31c1e750bbf36c1222fdf0ad7846dea62909
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2020-01-11 13:09:04 +0200

    missed @Controller annotationa added.

--- src/main/java/com/tekrajchhetri/blog/formdatasend/web/HomeController.java
+++ src/main/java/com/tekrajchhetri/blog/formdatasend/web/HomeController.java
@@ -1,9 +1,11 @@
 package com.tekrajchhetri.blog.formdatasend.web;
 
 import com.tekrajchhetri.blog.formdatasend.UserInfo;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class HomeController {
 
     @RequestMapping("/index")
